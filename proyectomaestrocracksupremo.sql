f020b5b6b8fd5b3206a56a95757de6de8177518c
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2020-11-05 21:13:31 -0500

    Add files via upload

---
+++ proyectomaestrocracksupremo.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-11-2020 a las 06:05:22
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyectomaestrocracksupremo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `NIT` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `X` double NOT NULL,
+  `Y` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materiaprima`
+--
+
+CREATE TABLE `materiaprima` (
+  `nombre` varchar(50) NOT NULL,
+  `cantidad` int(10) NOT NULL,
+  `fechaCaducidad` date NOT NULL,
+  `NIT_proveedor` varchar(50) NOT NULL,
+  `valorUnitario` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `materiaprima`
+--
+
+INSERT INTO `materiaprima` (`nombre`, `cantidad`, `fechaCaducidad`, `NIT_proveedor`, `valorUnitario`) VALUES
+('carne', 500, '2020-11-05', '455301', 2500),
+('papa', 250, '2020-11-05', '455301', 800);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `Nombre` varchar(50) NOT NULL,
+  `Cantidad` int(10) NOT NULL,
+  `Precio` double NOT NULL,
+  `FechaCaducidad` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`Nombre`, `Cantidad`, `Precio`, `FechaCaducidad`) VALUES
+('Empanada', 10, 80, '2020-11-12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `NIT` varchar(50) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Ubicacion` varchar(50) NOT NULL,
+  `Telefono` int(11) NOT NULL,
+  `Email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`NIT`, `Nombre`, `Ubicacion`, `Telefono`, `Email`) VALUES
+('455301', 'lalo', 'asdasd', 213214, 'asewqeqwe');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `receta`
+--
+
+CREATE TABLE `receta` (
+  `P` varchar(50) NOT NULL,
+  `M` varchar(50) NOT NULL,
+  `Cantidad` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `receta`
+--
+
+INSERT INTO `receta` (`P`, `M`, `Cantidad`) VALUES
+('Empanada', 'carne', 500),
+('Empanada', 'papa', 300);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recibo`
+--
+
+CREATE TABLE `recibo` (
+  `V` int(10) NOT NULL,
+  `C` varchar(50) NOT NULL,
+  `P` varchar(50) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vendedores`
+--
+
+CREATE TABLE `vendedores` (
+  `cedula` int(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `cargo` varchar(50) NOT NULL,
+  `comision` double NOT NULL,
+  `Telefono` int(10) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `sexo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`NIT`);
+
+--
+-- Indices de la tabla `materiaprima`
+--
+ALTER TABLE `materiaprima`
+  ADD PRIMARY KEY (`nombre`),
+  ADD KEY `NIT_proveedor` (`NIT_proveedor`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`Nombre`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`NIT`);
+
+--
+-- Indices de la tabla `receta`
+--
+ALTER TABLE `receta`
+  ADD PRIMARY KEY (`P`,`M`),
+  ADD KEY `M` (`M`);
+
+--
+-- Indices de la tabla `recibo`
+--
+ALTER TABLE `recibo`
+  ADD PRIMARY KEY (`V`,`C`,`P`,`Fecha`),
+  ADD KEY `C` (`C`),
+  ADD KEY `P` (`P`);
+
+--
+-- Indices de la tabla `vendedores`
+--
+ALTER TABLE `vendedores`
+  ADD PRIMARY KEY (`cedula`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `materiaprima`
+--
+ALTER TABLE `materiaprima`
+  ADD CONSTRAINT `materiaprima_ibfk_1` FOREIGN KEY (`NIT_proveedor`) REFERENCES `proveedor` (`NIT`);
+
+--
+-- Filtros para la tabla `receta`
+--
+ALTER TABLE `receta`
+  ADD CONSTRAINT `receta_ibfk_1` FOREIGN KEY (`P`) REFERENCES `producto` (`Nombre`),
+  ADD CONSTRAINT `receta_ibfk_2` FOREIGN KEY (`M`) REFERENCES `materiaprima` (`nombre`);
+
+--
+-- Filtros para la tabla `recibo`
+--
+ALTER TABLE `recibo`
+  ADD CONSTRAINT `recibo_ibfk_1` FOREIGN KEY (`V`) REFERENCES `vendedores` (`cedula`),
+  ADD CONSTRAINT `recibo_ibfk_2` FOREIGN KEY (`C`) REFERENCES `cliente` (`NIT`),
+  ADD CONSTRAINT `recibo_ibfk_3` FOREIGN KEY (`P`) REFERENCES `producto` (`Nombre`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
