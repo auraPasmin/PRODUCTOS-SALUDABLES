e563baef3275a4723eab7f0ab6155c4e442efda3
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2020-11-15 23:46:02 -0500

    Update Materia_Prima.java
    No contaba con todos los atributos

--- Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Materia_Prima.java
+++ Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Materia_Prima.java
@@ -7,54 +7,54 @@ public class Materia_Prima {
     private int cantidad;
     private LocalDate fechaCaducidad;
     private String nit_proveedor;
-
+    private double valor_unitario;
     public Materia_Prima() {
     }
 
-    public Materia_Prima(String nombre, int cantidad, LocalDate fechaCaducidad, String nit_proveedor) {
+    public Materia_Prima(String nombre, int cantidad, LocalDate fechaCaducidad, String nit_proveedor, double valor_unitario) {
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.fechaCaducidad = fechaCaducidad;
         this.nit_proveedor = nit_proveedor;
+        this.valor_unitario = valor_unitario;
     }
 
     public String getNombre() {
         return nombre;
     }
-
     public int getCantidad() {
         return cantidad;
     }
-
     public LocalDate getFechaCaducidad() {
         return fechaCaducidad;
     }
-
     public String getNit_proveedor() {
         return nit_proveedor;
     }
-
+    public double getValor_unitario() {
+        return valor_unitario;
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
-
     public void setFechaCaducidad(LocalDate fechaCaducidad) {
         this.fechaCaducidad = fechaCaducidad;
     }
-
-    
-
     public void setNit_proveedor(String nit_proveedor) {
         this.nit_proveedor = nit_proveedor;
     }
-
+    public void setValor_unitario(double valor_unitario) {
+        this.valor_unitario = valor_unitario;
+    }
+    
     @Override
     public String toString() {
-        return "Materia_Prima{ " + "nombre=" + nombre + ", cantidad=" + cantidad + ", fechaCaducidad=" + fechaCaducidad + ", nit_proveedor=" + nit_proveedor + '}';
+        return "Materia_Prima{" + "nombre=" + nombre + ", cantidad=" + cantidad + ", fechaCaducidad=" + fechaCaducidad + ", nit_proveedor=" + nit_proveedor + ", valor_unitario=" + valor_unitario + '}';
     }
+    
+
     
 }
