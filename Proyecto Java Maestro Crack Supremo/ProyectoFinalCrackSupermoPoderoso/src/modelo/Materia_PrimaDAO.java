b4d0530bb25c9cff39d5b555a2f48199712b3d3f
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2020-11-15 23:45:48 -0500

    Update Materia_PrimaDAO.java
    No contaba con todos los atributos

--- Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Materia_PrimaDAO.java
+++ Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Materia_PrimaDAO.java
@@ -1,14 +1,183 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
-/**
- *
- * @author Reynell
- */
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import servicios.Fachada;
+
 public class Materia_PrimaDAO {
-    
+        public Materia_PrimaDAO() {
+            // Nothing here
+        }
+
+    /**
+     * <strong>Crea</strong> y registra una materia prima la DB
+     * @param materia_prima, materia prima a registrar en la DB
+     * @return 1 si se creo exitosamente, 0 si no se realizaron
+     * cambios
+     * @see Class RecetaDAO
+     */
+    public int createMateria_Prima(Materia_Prima materia_prima) {
+        Connection conexion = null;
+        PreparedStatement instruccion = null;
+        int result = 0;
+        String sqlStatement;
+
+        try {
+            conexion = Fachada.startConnection();
+            sqlStatement = "INSERT INTO materiaprima VALUES (?, ?, ?, ?, ?)";
+            instruccion = conexion.prepareStatement(sqlStatement);
+
+            instruccion.setString(1,materia_prima.getNombre());
+            instruccion.setInt(2, materia_prima.getCantidad());
+            instruccion.setDate(3, Date.valueOf(materia_prima.getFechaCaducidad()));
+            instruccion.setString(4, materia_prima.getNit_proveedor());
+            instruccion.setInt(5, materia_prima.ge);
+            result = instruccion.executeUpdate();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            try {
+                if(instruccion != null)
+                    instruccion.close();
+                if(conexion != null)
+                    conexion.close();
+            }
+            catch(SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+        return result;
+    }
+
+    /**
+     * <strong>Lee</strong> de la DB las recetas
+     * @return <code>ArrayList<Receta></code> una lista
+     * de las recetas.
+     * @see Class recetaDAO
+     */
+    public ArrayList<Receta> readRecetas() {
+        Connection conexion = null;
+        PreparedStatement instruccion = null;
+        ArrayList<Receta> listarReceta = null;
+        ResultSet resultado = null;
+        String sqlStatement;
+
+        try {
+            listarReceta = new ArrayList<>();
+            conexion = Fachada.startConnection();
+            sqlStatement = "SELECT * FROM receta ORDER BY P";
+            instruccion = conexion.prepareStatement(sqlStatement);
+            resultado = instruccion.executeQuery();
+
+            while(resultado.next()) {
+                Receta receta = new Receta();
+
+                receta.setP(resultado.getString(1));
+                receta.setM(resultado.getString(2));
+                receta.setCantidad(resultado.getInt(3));
+
+                listarReceta.add(receta);
+            }
+        }
+        catch(SQLException e) {
+            // Do something
+        }
+        finally {
+            try {
+                if(instruccion != null)
+                    instruccion.close();
+                if(conexion != null)
+                    conexion.close();
+            }
+            catch(SQLException ex) {
+                // Do something
+            }
+        }
+        return listarReceta;
+    }
+
+    /**
+     * <strong>Actualiza</strong> una receta de la DB
+     * @param receta, una receta a ser cambiada
+     * @return 1 si se realizo la actualizacion, 0 si
+     * no se realizo ningun cambio
+     * @see Class RecetaDAO
+     */
+    public int updateReceta(Receta receta) {
+        Connection conexion = null;
+        PreparedStatement instruccion = null;
+        int resultado = 0;
+        String sqlStatement;
+
+        try {
+            conexion = Fachada.startConnection();
+            sqlStatement = "UPDATE receta M = ?, Cantidad = ? WHERE P = ?";
+            instruccion = conexion.prepareStatement(sqlStatement);
+
+            instruccion.setString(1,receta.getM());
+            instruccion.setInt(2, receta.getCantidad());
+            instruccion.setString(3, receta.getP());
+
+            resultado = instruccion.executeUpdate();
+        }
+        catch(SQLException e) {
+            // Do something ...
+        }
+        finally {
+            try {
+                if(instruccion != null)
+                    instruccion.close();
+                if(conexion != null)
+                    conexion.close();
+            }
+            catch (SQLException ex) {
+                // Do something ...
+            }
+        }
+        return resultado;
+    }
+
+    /**
+     * <strong>Borra</strong> una receta de un producto
+     * registrado en la DB
+     * @param p, la receta del producto a ser eliminada
+     * @return 1 si se realizo la eliminacion correctamente,
+     * 0 si no se realizo nada
+     * @see Class RecetaDAO
+     */
+    public int deleteReceta(String p) {
+        Connection conexion = null;
+        PreparedStatement instruccion = null;
+        int resultado = 0;
+        String sqlstatement;
+        try {
+            conexion = Fachada.startConnection();
+            sqlstatement = "DELETE FROM receta WHERE P = ?";
+            instruccion = conexion.prepareStatement(sqlstatement);
+
+            instruccion.setString(1, p);
+            resultado = instruccion.executeUpdate();
+        }
+        catch(SQLException e) {
+            // Do something ...
+        }
+        finally {
+            try {
+                if(instruccion != null)
+                    instruccion.close();
+                if(conexion != null)
+                    conexion.close();
+            }
+            catch (SQLException ex) {
+                // Do something
+            }
+        }
+        return resultado;
+    }
 }
