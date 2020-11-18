61d9e99b5c6e542a19048bb213785053adf4bd36
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2020-11-18 11:26:16 -0500

    Update Pruebas.java

--- Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Pruebas.java
+++ Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Pruebas.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 
 public class Pruebas {
     public static void main(String[]args){
-        LocalDate t = LocalDate.now();
-        System.out.println(t.plusDays(1));
+        ReciboDAO r = new ReciboDAO();
     }
 }
