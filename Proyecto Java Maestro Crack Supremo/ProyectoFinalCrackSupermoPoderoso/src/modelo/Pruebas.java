635b51065b318bc4a2dd9cb949d9ad966a753844
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2020-11-21 21:14:43 -0500

    ACC

--- Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Pruebas.java
+++ Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Pruebas.java
@@ -12,16 +12,8 @@ import javax.swing.JOptionPane;
 
 public class Pruebas {
     public static void main(String[]args) throws NEDException{
-        LocalDate t = LocalDate.of(2020, 11, 01);
-        t.plusMonths(1);
-        System.out.println(t.plusMonths(1));
-        RecetaDAO v = new RecetaDAO();
+
         ProductoDAO p = new ProductoDAO();
-        
-        
-        Object[][]data = v.generarReceta(p.cargarProducto("paparellena"));
-        for(Object[] d1 : data){
-            System.out.println(Arrays.toString(d1));
-        }
+        System.out.println(p.reabastecerProducto("paparellena", 3));
     }
 }
