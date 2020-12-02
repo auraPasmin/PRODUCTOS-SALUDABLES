64e9f2577f049aa4d193dc8156fea6b494e76999
ReynellQ <73651961+ReynellQ@users.noreply.github.com>
2020-12-01 20:19:36 -0500

    acc

--- Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Pruebas.java
+++ Proyecto Java Maestro Crack Supremo/ProyectoFinalCrackSupermoPoderoso/src/modelo/Pruebas.java
@@ -3,30 +3,13 @@ package modelo;
 
 import controladores.ControladorCCliente;
 import controladores.ControladorCVendedor;
+import controladores.ControladorCruds;
 import controladores.ControladorGVenta;
 
 
 public class Pruebas {
     public static void main(String[]args) throws NEDException{
-/*
-        Materia_PrimaDAO m = new Materia_PrimaDAO();
-        m.crearMP(new Materia_Prima());
-*/      
-
-        /**
-         * Prueba reynell
-         *
-        AutomaticFillerMP ATMP = new AutomaticFillerMP();
-        Thread hiloATMP = new Thread(ATMP);
-        hiloATMP.setDaemon(true);
-        ATMP.start();
-        * 
-        * /*/
-        
-        // Prueba Santiago
-        //ControladorCVendedor cv = new ControladorCVendedor("master");
-        VendedorDAO v = new VendedorDAO();
-        ControladorGVenta cc = new ControladorGVenta(v.cargarVendedor(456412));        
+        ControladorCruds c = new ControladorCruds(6);
         
     }
 }
